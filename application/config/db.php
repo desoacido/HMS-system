af5b43ef9a332cb67574ddecab3a70a6191fcfd5
desoacido <desoacidosweetangel5@gmail.com>
2026-04-28 14:39:37 +0800

    Update db.php

--- application/config/db.php
+++ application/config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$dbname = "hms";
-$username = "root";
-$password = "";
+$host     = "sql12.freesqldatabase.com"; // ← palitan ng actual host nila
+$dbname   = "sql12824630";              // ← palitan ng actual db name
+$username = "sql12824630";              // ← palitan ng actual username
+$password = "P3zrdTXyaX";            // ← palitan ng actual password
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
