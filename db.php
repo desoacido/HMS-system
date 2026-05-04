7ca3c1e2d857a4736825147907923f65b4e057d0
desoacido <desoacidosweetangel5@gmail.com>
2026-05-04 19:10:38 +0800

    Update db.php

--- db.php
+++ db.php
@@ -11,5 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+    date_default_timezone_set('Asia/Manila');
 // echo "Connected successfully"; 
 ?>
