564e0a286a512cfd938e642bd2ff85ac92a1dd7f
desoacido <desoacidosweetangel5@gmail.com>
2026-05-04 10:26:00 +0800

    delete files

--- presentation/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-
-// redirect to login page
-header("Location: login.php");
-exit();
-?>
--- test_qr.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'phpqrcode/qrlib.php';
-
-QRcode::png("TEST QR HMS");
-?>
